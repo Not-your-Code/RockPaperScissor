d199d350dcf6704a5dfbb1a978ca34d8fd5fcfc6
Vineet Agarwal <vineetagarwal.now@gmail.com>
2021-11-11 19:19:43 +0530

    Some score functionality added plus worked on animation ,no much of an UI thing but yeah looks fine for now

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 let rulebtn = document.getElementById('rules')
 rulebtn.addEventListener('click' , ()=>{
        document.querySelector(".Rulebox").style.visibility = "visible"
-})
+       document.getElementById('lightbox').style.visibility = "visible"
+    })
 let cross = document.getElementById("Cross")
 cross.addEventListener('click' , ()=>{
+    document.getElementById('lightbox').style.visibility = "hidden"
     document.querySelector(".Rulebox").style.visibility = "hidden"
     
+})
+
+let play = document.getElementById('playnow')
+play.addEventListener('click' , ()=>{
+    document.querySelector('.score').classList.add("scactive")
+    play.style.visibility="hidden"
+     
 })
